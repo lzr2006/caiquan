4c3216bcacca5f8b19e6e3e5e67c7531562b5bff
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2021-02-04 17:00:39 +0800

    Add files via upload

---
+++ caiquan/Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caiquan
+{
+    class Core
+    {
+
+        public int winner(int user, int computer1, int computer2) {
+            //传入值1代表剪刀，2代表石头，3代表布
+            //返回值规范:0->平局 1->Victor单独胜利 2->Utanus单独胜利 3->Victor与user同时胜利 4->Utanus与user同时胜利 5->Victor与Utanus同时胜利 6->user单独胜利
+
+        }
+    }
+}
